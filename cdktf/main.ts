992eac534b157f5c83b8dedaa93daf72659c60c4
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-06-23 00:20:53 +0000

    Firebase project added.

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,10 +1,15 @@
 import { Construct } from "constructs";
-import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { App, TerraformStack } from "cdktf";
 import { GoogleBetaProvider } from "./.gen/providers/google-beta/provider/index";
 import { GoogleFirebaseProject } from "./.gen/providers/google-beta/google-firebase-project";
 import { GoogleFirebaseWebApp } from "./.gen/providers/google-beta/google-firebase-web-app";
-import { DataGoogleFirebaseWebAppConfigA } from "./.gen/providers/google-beta/data-google-firebase-web-app-config";
 
+import { GoogleFirestoreDatabase } from "./.gen/providers/google-beta/google-firestore-database";
+import { GoogleFirebaserulesRuleset } from "./.gen/providers/google-beta/google-firebaserules-ruleset";
+import { GoogleFirebaserulesRelease } from "./.gen/providers/google-beta/google-firebaserules-release";
+
+import { GoogleIdentityPlatformConfig } from "./.gen/providers/google-beta/google-identity-platform-config";
+import { GoogleIdentityPlatformProjectDefaultConfig } from "./.gen/providers/google-beta/google-identity-platform-project-default-config";
 
 
 import { GoogleProject } from "./.gen/providers/google-beta/google-project";
@@ -13,23 +18,29 @@ import { ProjectInfo } from "./process_object";
 import { DataGoogleBillingAccount } from "./.gen/providers/google-beta/data-google-billing-account";
 import { GoogleProjectService } from "./.gen/providers/google-beta/google-project-service";
 
+import { GoogleProjectIamPolicy } from "./.gen/providers/google-beta/google-project-iam-policy";
+import { DataGoogleIamPolicy } from "./.gen/providers/google-beta/data-google-iam-policy";
+
+
+
 class VertexAiChatAppStack extends TerraformStack {
   constructor(scope: Construct, id: string, projectInfo: ProjectInfo) {
     super(scope, id);
     // new GoogleProvider(this, `provider-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, projectInfo.projectMeta);
-    // const googleBetaProvider = 
-    new GoogleBetaProvider(this, `beta-provider-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, {
+    const googleBetaProvider = new GoogleBetaProvider(this, `beta-provider`, {
       alias: "no_user_project_override",
-      userProjectOverride: false
+      userProjectOverride: true,
+      region: projectInfo.projectMeta?.region,
     });
 
     const billingAccount = new DataGoogleBillingAccount(this, "billing-account", {
       billingAccount: projectInfo.projectMeta?.billingAccount,
     });
 
-    const project = new GoogleProject(this, `project-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, {
+    const project = new GoogleProject(this, `project`, {
       name: projectInfo.projectMeta?.project!,
       projectId: projectInfo.projectMeta?.project!,
+
       billingAccount: billingAccount.id,
       skipDelete: false,
       labels: {
@@ -42,6 +53,8 @@ class VertexAiChatAppStack extends TerraformStack {
       "cloudresourcemanager.googleapis.com",
       "firebase.googleapis.com",
       "serviceusage.googleapis.com",
+      "firestore.googleapis.com",
+      "firebaserules.googleapis.com",
     ];
     const services = [];
     for (const api of apis) {
@@ -52,41 +65,130 @@ class VertexAiChatAppStack extends TerraformStack {
       }));
     }
 
-    const googleFirebaseProject = new GoogleFirebaseProject(this, `firebase-project-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, {
-      project: project.id,      
-      // provider: googleBetaProvider,
-      dependsOn: [project]
+    const dataGoogleIamPolicy = new DataGoogleIamPolicy(this, `iam-policy`, {
+      binding: [{ role: "roles/owner", members: [`user:cy.gdoc@gmail.com`] }]
     })
 
-    const googleFirebaseWebApp = new GoogleFirebaseWebApp(this, `firebase-web-app-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, {
+    const googleProjectIamPolicy = new GoogleProjectIamPolicy(this, `iam-project`, {
       project: project.id,
-      displayName: "Vertex AI Chat Firebase Web App",      
-      deletionPolicy: "DELETE",
-      // provider: googleBetaProvider,
+      policyData: dataGoogleIamPolicy.policyData
+    });
+
+    const googleFirebaseProject = new GoogleFirebaseProject(this, `firebase-project`, {
+      project: project.name,
+      provider: googleBetaProvider,
+      dependsOn: [...services, project, googleProjectIamPolicy]
+    })
+
+    const googleIdentityPlatformConfig = new GoogleIdentityPlatformConfig(this, `identity-platform-config`, {
+      project: project.name,
+      provider: googleBetaProvider,
+      autodeleteAnonymousUsers: true,
       dependsOn: [googleFirebaseProject],
     })
 
-    const googleFirebaseWebAppConfig = new DataGoogleFirebaseWebAppConfigA(this,
-      `firebase-web-app-config-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`, {
-      webAppId: googleFirebaseWebApp.appId,
-      // provider: googleBetaProvider,
+    new GoogleIdentityPlatformProjectDefaultConfig(this, `identity-platform-default-config`, {
+      project: project.name,
+      provider: googleBetaProvider,
+      signIn: {
+        allowDuplicateEmails: false,
+        anonymous: {
+          enabled: true
+        },
+        email: {
+          enabled: true,
+          passwordRequired: false,
+        }
+      },
+      dependsOn: [googleIdentityPlatformConfig],
     })
 
-    new TerraformOutput(this, "appId", {
-      value: googleFirebaseWebApp.appId,
+    // const googleFirebaseWebApp =     
+    new GoogleFirebaseWebApp(this, `firebase-web-app`, {
+      project: project.name,
+      displayName: "Vertex AI Chat Firebase Web App",
+      deletionPolicy: "DELETE",
+      provider: googleBetaProvider,
+      dependsOn: [googleFirebaseProject],
     });
-    new TerraformOutput(this, "apiKey", {
-      value: googleFirebaseWebAppConfig.apiKey,
+
+    const googleFirestoreDatabase = new GoogleFirestoreDatabase(this, `firestore-database`, {
+      project: project.name,
+      provider: googleBetaProvider,
+      name: "default",
+      locationId: projectInfo.projectMeta?.region!,
+      type: "FIRESTORE_NATIVE",
+      concurrencyMode: "OPTIMISTIC",
+      dependsOn: [googleFirebaseProject],
     });
-    new TerraformOutput(this, "authDomain", {
-      value: googleFirebaseWebAppConfig.authDomain,
+
+    const googleFirebaserulesRuleset = new GoogleFirebaserulesRuleset(this, `firebaserules-ruleset`, {
+      project: project.name,
+      provider: googleBetaProvider,      
+      source: {
+        files: [{
+          name: "firestore.rules",
+          content: `service cloud.firestore {
+            match /databases/{database}/documents {
+              // Messages:
+              //   - Anyone can read.
+              //   - Authenticated users can add and edit messages.
+              //   - Validation: Check name is same as auth token and text length below 300 char or that imageUrl is a URL.
+              //   - Deletes are not allowed.
+              match /messages/{messageId} {
+                allow read;
+                allow create, update: if request.auth != null
+                              && request.resource.data.name == request.auth.token.name
+                              && (request.resource.data.text is string
+                                && request.resource.data.text.size() <= 300
+                                || request.resource.data.imageUrl is string
+                                && request.resource.data.imageUrl.matches('https?://.*'));
+                allow delete: if false;
+              }
+              // FCM Tokens:
+              //   - Anyone can write their token.
+              //   - Reading list of tokens is not allowed.
+              match /fcmTokens/{token} {
+                allow read: if false;
+                allow write;
+              }
+            }
+          }
+          `
+        }]
+      },
+      dependsOn: [googleFirestoreDatabase],
     });
-    new TerraformOutput(this, "databaseURL", {
-      value: googleFirebaseWebAppConfig.getStringAttribute("database_url"),
+
+    new GoogleFirebaserulesRelease(this, `firebaserules-release`, {
+      project: project.name,
+      provider: googleBetaProvider,
+      name: "firestore.rules",
+      rulesetName: googleFirebaserulesRuleset.name,
+      lifecycle: {
+        replaceTriggeredBy: [googleFirebaserulesRuleset.name]
+      },
+      dependsOn: [googleFirestoreDatabase],
     });
 
 
-    // define resources here
+    // const googleFirebaseWebAppConfig = new DataGoogleFirebaseWebAppConfigA(this, `firebase-web-app-config`, {
+    //   webAppId: googleFirebaseWebApp.appId,
+    //   provider: googleBetaProvider,
+    // })
+
+    // new TerraformOutput(this, "appId", {
+    //   value: googleFirebaseWebApp.appId,
+    // });
+    // new TerraformOutput(this, "apiKey", {
+    //   value: googleFirebaseWebAppConfig.apiKey,
+    // });
+    // new TerraformOutput(this, "authDomain", {
+    //   value: googleFirebaseWebAppConfig.authDomain,
+    // });
+    // new TerraformOutput(this, "databaseURL", {
+    //   value: googleFirebaseWebAppConfig.getStringAttribute("database_url"),
+    // });
   }
 }
 
